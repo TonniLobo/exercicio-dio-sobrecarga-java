4bb3d7c98b806feff3b60bde659b34403b523f03
TonniLobo <grohli18@hotmail.com>
2022-03-06 22:36:57 -0300

    Atualização - exercício retornos

--- src/br/com/dio/Main.java
+++ src/br/com/dio/Main.java
@@ -2,10 +2,13 @@ package br.com.dio;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Exercício quadrilátero");
-        Quadrilatero.area(3);
-        Quadrilatero.area(5d,5d);
-        Quadrilatero.area(7,8,9);
-        Quadrilatero.area(5f,5f);
+        System.out.println("Exercício Retornos");
+
+        double areaQuadrado= Quadrilatero.area(3);
+        double areaRetangulo= Quadrilatero.area(5,5);
+        double areaTrapezio= Quadrilatero.area(7,8,9);
+        System.out.println("Área do Quadrado: "+areaQuadrado);
+        System.out.println("Área do Retângulo: "+areaRetangulo);
+        System.out.println("Área do Trapézio: "+areaTrapezio);
     }
 }
